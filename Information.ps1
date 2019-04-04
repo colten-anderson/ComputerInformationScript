d39681d64e34c012f9a6ae7f07eab7116925d330
colten-anderson <49244321+colten-anderson@users.noreply.github.com>
2019-04-04 09:22:29 -0500

    Update Information.ps1

--- Information.ps1
+++ Information.ps1
@@ -1,9 +1,55 @@
 ﻿#### Variables ####
 
-    $computerName = Read-Host -Prompt 'Input computer name'
     $baddevices = Get-WmiObject Win32_PNPEntity | where {$_.ConfigManagerErrorcode -ne 0}
     $colItems = Get-WmiObject -Class Win32_USBHub
     $colSoftware = Get-WmiObject -Class Win32_Product | Where-Object {$_.IdentifyingNumber -eq "{90280409-6000-11D3-8CFE-0050048383C9}"}
+	
+### Opens input form for computer name ###
+
+	Add-Type -AssemblyName System.Windows.Forms
+	Add-Type -AssemblyName System.Drawing
+
+	$form = New-Object System.Windows.Forms.Form
+	$form.Text = 'Data Entry Form'
+	$form.Size = New-Object System.Drawing.Size(300,200)
+	$form.StartPosition = 'CenterScreen'
+
+	$OKButton = New-Object System.Windows.Forms.Button
+	$OKButton.Location = New-Object System.Drawing.Point(75,120)
+	$OKButton.Size = New-Object System.Drawing.Size(75,23)
+	$OKButton.Text = 'OK'
+	$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+	$form.AcceptButton = $OKButton
+	$form.Controls.Add($OKButton)
+
+	$CancelButton = New-Object System.Windows.Forms.Button
+	$CancelButton.Location = New-Object System.Drawing.Point(150,120)
+	$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+	$CancelButton.Text = 'Cancel'
+	$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+	$form.CancelButton = $CancelButton
+	$form.Controls.Add($CancelButton)
+
+	$label = New-Object System.Windows.Forms.Label
+	$label.Location = New-Object System.Drawing.Point(10,20)
+	$label.Size = New-Object System.Drawing.Size(280,20)
+	$label.Text = 'Please enter the Computer Name in the space below:'
+	$form.Controls.Add($label)
+
+	$textBox = New-Object System.Windows.Forms.TextBox
+	$textBox.Location = New-Object System.Drawing.Point(10,40)
+	$textBox.Size = New-Object System.Drawing.Size(260,20)
+	$form.Controls.Add($textBox)
+
+	$form.Topmost = $true
+
+	$form.Add_Shown({$textBox.Select()})
+	$result = $form.ShowDialog()
+
+	if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+	{
+		$computerName = $textBox.Text
+	}
 
 ### Get Information and Display It ###
 
